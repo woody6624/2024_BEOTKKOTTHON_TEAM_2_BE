eeae593603c5eed1cf81717dc676aacea2b99792
woody6624 <woody6624@gmail.com>
2024-03-13 02:14:18 +0900

    테스트

---
+++ src/main/java/muckkitlist_spring/muckkitlist_spring/MuckkitlistSpringApplication.java
@@ -0,0 +1,13 @@
+package muckkitlist_spring.muckkitlist_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuckkitlistSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuckkitlistSpringApplication.class, args);
+	}
+
+}
